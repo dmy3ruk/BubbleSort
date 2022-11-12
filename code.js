c5275eeb3dab018e48317cd40d0b826fa60641fc
dmy3ruk <104530429+dmy3ruk@users.noreply.github.com>
2022-11-12 16:03:49 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+const mas = [4, 7, 2, 6, 5, 1];
+for (var i=0; i<mas.length-1; i++){
+    for (var j=0; j<mas.length-1; j++){
+        if (mas[j]>mas[j+1]){
+        let a=mas[j];
+        mas[j]=mas[j+1];
+        mas[j+1]=a;
+    }
+}
+}
+console.log(mas)
